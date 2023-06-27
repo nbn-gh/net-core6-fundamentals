448be2a4b696867e5fac1de3fcf2bf46e0ae808d
Nabin Ghimire <nabin0723@gmail.com>
2023-06-26 23:54:06 -0400

    added dto, added dummy data and returning.

---
+++ CitiInfo/CitiInfo.API/CitiesDataStore.cs
@@ -0,0 +1,24 @@
+﻿using CitiInfo.API.Models;
+
+namespace CitiInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        // Added the static property to retrun this class. this is part of singleton pattern network
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            // init dummy data
+            Cities = new List<CityDto>()
+            {
+                new CityDto() { Id = 1, Name = "New York City", Description="The one with big park."},
+                new CityDto() { Id = 2, Name = "Antwrep", Description="The one with the cathedral that was never really finished."},
+                new CityDto() { Id = 3, Name = "Paris", Description = "The one with the big tower"}
+            };
+        }
+
+    }
+}
--- CitiInfo/CitiInfo.API/Controllers/CitiesController.cs
+++ CitiInfo/CitiInfo.API/Controllers/CitiesController.cs
@@ -4,17 +4,19 @@ namespace CitiInfo.API.Controllers
 {
     [ApiController]
     [Route("api/cities")]
-    public class CitiesController: ControllerBase
+    public class CitiesController : ControllerBase
     {
         [HttpGet]
-         public JsonResult GetCities()
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
         {
             return new JsonResult(
-                new List<object>
-                {
-                    new { id = 1, Name = "New York City"},
-                    new { id = 2, Name = "Antwerp"}
-                });
-        } 
+                CitiesDataStore.Current.Cities.FirstOrDefault( c => c.Id == id));
+        }
     }
 }
