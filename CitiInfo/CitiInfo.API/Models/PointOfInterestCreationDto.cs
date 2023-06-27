b40b9ffdc881e1e30d4698a99b57f443be5c6acd
Nabin Ghimire <nabin0723@gmail.com>
2023-06-27 11:53:21 -0400

    added HttpPost method

---
+++ CitiInfo/CitiInfo.API/Models/PointOfInterestCreationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CitiInfo.API.Models
+{
+    public class PointOfInterestCreationDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
