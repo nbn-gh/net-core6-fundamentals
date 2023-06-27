7d7e80db0aa1ac89e37b09e709cdfb8e461f4cf0
Nabin Ghimire <nabin0723@gmail.com>
2023-06-27 16:43:30 -0400

    data annotation validation

--- CitiInfo/CitiInfo.API/Models/PointOfInterestCreationDto.cs
+++ CitiInfo/CitiInfo.API/Models/PointOfInterestCreationDto.cs
@@ -1,8 +1,14 @@
-﻿namespace CitiInfo.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CitiInfo.API.Models
 {
     public class PointOfInterestCreationDto
     {
+        [Required(ErrorMessage ="you should provide a name value")]
+        [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
+        
+        [MaxLength(100)]
         public string? Description { get; set; }
     }
 }
