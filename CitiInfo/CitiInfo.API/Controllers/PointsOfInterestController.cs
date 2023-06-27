d8abc8a39be317b6dc02b80ffe5c271e4971e03d
Nabin Ghimire <nabin0723@gmail.com>
2023-06-27 00:23:57 -0400

    added child resources PointsOfInterest

---
+++ CitiInfo/CitiInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,46 @@
+﻿using CitiInfo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CitiInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/[controller]")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var cityInfo = CitiesDataStore.Current.Cities
+                .FirstOrDefault(c => c.Id == cityId);
+
+            if (cityInfo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityInfo.PointsOfInterest);
+        }
+
+        [HttpGet("{pointOfInterestId}")]
+        public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
+        {
+            var cityInfo = CitiesDataStore.Current.Cities
+                .FirstOrDefault(c => c.Id == cityId);
+
+            if (cityInfo == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = cityInfo.PointsOfInterest
+                .FirstOrDefault(p => p.Id == pointOfInterestId);
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
+    }
+}
