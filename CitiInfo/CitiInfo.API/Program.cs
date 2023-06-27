90079c36b440d50c342d154eb1f9cc0f09cc35c7
Nabin Ghimire <35967140+nabin0723@users.noreply.github.com>
2023-06-27 11:21:45 -0400

    content negotiation and file download (#7)

--- CitiInfo/CitiInfo.API/Program.cs
+++ CitiInfo/CitiInfo.API/Program.cs
@@ -1,11 +1,21 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// for supporting file type
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
