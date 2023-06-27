e74eec99ed0772362fba938ca8e2264a0328f210
Nabin Ghimire <35967140+nabin0723@users.noreply.github.com>
2023-06-26 23:55:25 -0400

    Registering api service (#4)
    * added app.UseRouting and UseEndpoints on program cs file.  added CitiesController
    
    * added dto, added dummy data and returning.

---
+++ CitiInfo/CitiInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CitiInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(
+                CitiesDataStore.Current.Cities.FirstOrDefault( c => c.Id == id));
+        }
+    }
+}
