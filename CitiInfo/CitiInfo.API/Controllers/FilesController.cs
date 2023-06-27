b453222a64be8dd7c20a0a32cebe9d816a35e717
Nabin Ghimire <nabin0723@gmail.com>
2023-06-27 11:21:09 -0400

    content negotiation and file download

---
+++ CitiInfo/CitiInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CitiInfo.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        public FileExtensionContentTypeProvider _fileExtensionContentTypeProvider { get; }
+
+        public FilesController(
+            FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider ??
+                throw new System.ArgumentNullException(nameof(FileExtensionContentTypeProvider));
+        }
+
+
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            var pathToFile = "path-to-file";
+
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if(!_fileExtensionContentTypeProvider.TryGetContentType(pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+
+        }
+    }
+}
