1b644f2cba0ac7a967e6a104b52447d9ec9b8ce4
Nabin Ghimire <nabin0723@gmail.com>
2023-06-28 00:05:22 -0400

    using appsetting.json dev environment as the config information. IConfiguration implementation

--- CitiInfo/CitiInfo.API/Services/CloudMailService.cs
+++ CitiInfo/CitiInfo.API/Services/CloudMailService.cs
@@ -2,8 +2,15 @@
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@mycompany.com";
+        private readonly string _mailTo = string.Empty;
+        private readonly string _mailFrom = string.Empty;
+
+        public CloudMailService(IConfiguration configuration)
+        {
+                _mailTo = configuration["mailSettings:mailToAddress"];
+                _mailFrom = configuration["mailSettings:mailFromAddress"];
+        }
+
         public void Send(string subject, string message)
         {
             Console.WriteLine("************* Sending Mail From Cloud Service *************");
