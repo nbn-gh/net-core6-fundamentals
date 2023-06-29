f27f3d625b810b7509ffbe3b5f609063a4eb4d4e
Nabin Ghimire <nabin0723@gmail.com>
2023-06-28 23:22:19 -0400

    Added Entity Frame Core and Sqlite. Added DbContext

---
+++ CitiInfo/CitiInfo.API/DbContexts/CityInfoContext.cs
@@ -0,0 +1,16 @@
+﻿using CitiInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CitiInfo.API.DbContexts
+{
+    public class CityInfoContext : DbContext
+    {
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<City> Cities { get; set; } = null!; // null forgiving operator
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
+    }
+}
