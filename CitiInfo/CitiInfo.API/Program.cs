26a6b5a6c10bf9e9d7465ce7cf2e0fbe3b93876b
Nabin Ghimire <35967140+nabin0723@users.noreply.github.com>
2023-07-03 13:29:21 -0400

    Securing api (#17)
    * generating a token
    
    * setting up middleware for using authentication and implementing Authorize attribute
    
    * implemented authorization policy

--- CitiInfo/CitiInfo.API/Program.cs
+++ CitiInfo/CitiInfo.API/Program.cs
@@ -3,7 +3,9 @@ using CitiInfo.API.DbContexts;
 using CitiInfo.API.Services;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
+using System.Text;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
@@ -47,10 +49,33 @@ builder.Services.AddDbContext<CityInfoContext>(dbContextOptions =>
 );
 
 builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
-
-
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Authentication:Issuer"],
+            ValidAudience = builder.Configuration["Authentication:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Authentication:SecretForKey"]))
+        };
+    });
+
+
+// For Authorization Policy
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MustBeFromAntwrep", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("city", "Antwrep");
+    });
+
+});
 
 var app = builder.Build();
 
@@ -66,6 +91,7 @@ app.UseHttpsRedirection();
 // Marks the position in the middleware pipeline where a routing decision is made
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 // Marks the position in the middleware pipeline where the selected endpoint is executed
